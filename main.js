da59092a23ca09012e52fe87b7f41c8d89db0a49
LorenaAcunaG <lorena.garavito.itg@gmail.com>
2023-01-30 11:39:42 -0500

    Ejercicio completado

---
+++ main.js
@@ -0,0 +1,53 @@
+alert('Ingresa tres numeros')
+
+let num1 = Number(prompt("Ingrese el primer numero"));
+let num2 = Number(prompt("Ingrese el segundo numero"));
+let num3 = Number(prompt("Ingrese el tercer numero"));
+let numerosArray = [];
+
+if(num1!=num2&&num2!=num3&&num1!=num3){
+    //los numeros son diferentes
+    if(num1 < num2 && num2 < num3){
+        let numerosArray = [num1, num2, num3];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+
+    }else if(num1 < num2 && num3 < num2 ){
+        let numerosArray = [num1, num3, num2];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+
+    }else if(num2 < num1 && num1 < num3 ){
+        let numerosArray = [num2, num1, num3];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+
+    }else if(num2 < num1 && num3 < num1 ){
+        let numerosArray = [num2, num3, num1];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+
+    }else if(num3 < num1 && num1 < num2 ){
+        let numerosArray = [num3, num1, num2];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+
+    }else{
+        let numerosArray = [num3, num2, num1];
+        console.log (`Los numeros odenados de menor a mayor son: ${numerosArray}`);
+        console.log (`Los numeros odenados de mayor a menor son: ${numerosArray.reverse()}`);
+    }
+
+}else if(num1===num2){
+
+        console.log(`los numeros 1 y 2 ingresados son iguales`);
+
+    }else if (num2===num3){
+
+        console.log(`El numero 2 y el numero 3 ingresados son iguales`);
+
+}else{
+
+    console.log(`El numero 1 y el numero 3 ingresados son iguales`);
+}
+
